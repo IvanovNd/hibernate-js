b4843669fd7e1c23e0f2eb2e7b7b81cb19922430
IvanovND <ivanovndspb@gmail.com>
2017-10-11 23:54:14 +0300

    add hibernate config and start point

---
+++ src/main/java/js/AppMain.java
@@ -0,0 +1,18 @@
+package js;
+
+import js.hibernate.HibernateUtil;
+import org.hibernate.Session;
+
+public class AppMain {
+    public static void main(String[] args) {
+
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+        System.out.println("Work");
+        session.getTransaction().commit();
+        session.close();
+
+        HibernateUtil.shutdown();
+
+    }
+}
